df268dc9ceba587f61d8f5a392cbb8c8ff7c217d
Zakaria <Zakaria.ALIZOUAOUI.dz@viacesi.fr>
2024-06-25 01:03:53 +0100

    Fix dockerfiles, docker-compose.yml

--- microservices/api-service/backend/src/healthcheck.ts
+++
@@ -1,6 +0,0 @@
-import express from 'express';
-const router = express.Router();
-router.get('/healthcheck', (req, res) => {
-  res.send('OK');
-});
-export default router;
---
+++ microservices/auth-service/backend/index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3010
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
+require("dotenv").config();
